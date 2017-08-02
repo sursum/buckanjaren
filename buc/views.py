7219bdd1d362dd7aa93ec8d5e1a2137640a21e5e
sursum <chrismakoundoul@gmail.com>
2017-08-02 11:48:06 +0200

    Update views.py

--- buc/views.py
+++ buc/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, TemplateView
-from buc.models import Category, Article
+from buc.models import Category, Article, Tag
 
 # Create your views here.
 class HomeView(TemplateView):
